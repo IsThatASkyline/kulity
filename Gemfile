6c74681a2fb4264f18712216a9eba043303c222d
artklk12 <artem-kulik@list.ru>
2021-03-06 00:44:33 +0300

    initial

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby "3.0.0"
+
+gem "telegram-bot-ruby", "~> 0.10"
+gem "activerecord", "~> 5.2"
+gem 'i18n'
+gem "pg", "~> 1.1"
+gem "rake", "~> 12.3"
---
+++ vendor/bundle/ruby/3.0.0/gems/telegram-bot-ruby-0.5.0/lib/telegram/bot/types/update.rb
@@ -0,0 +1,13 @@
+module Telegram
+  module Bot
+    module Types
+      class Update < Base
+        attribute :update_id, Integer
+        attribute :message, Message
+        attribute :inline_query, InlineQuery
+        attribute :chosen_inline_result, ChosenInlineResult
+        attribute :callback_query, CallbackQuery
+      end
+    end
+  end
+end
